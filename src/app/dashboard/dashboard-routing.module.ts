80d74b2126aef8db9e8e60732d80272b22686d78
TicoYk <didimoykym@gmail.com>
2021-01-06 18:46:58 -0300

    criado componente e template da dashboard

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,9 @@
+import { DashboardComponent } from './dashboard.component';
+import { Routes } from '@angular/router';
+
+export const DashBoardRoutes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
