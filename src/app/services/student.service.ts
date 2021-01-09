6d51daf2fef0618bb9f7dd2e740242fabac11e29
TicoYk <didimoykym@gmail.com>
2021-01-08 21:42:58 -0300

    servico para estudante criado

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,4 +1,5 @@
-import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import Student from '../aluno/shared/student.model';
 import { Observable } from 'rxjs';
@@ -9,7 +10,16 @@ export class StudentService {
   readonly apiURL: string;
 
   constructor(private http: HttpClient) {
-    this.apiURL = 'http://localhost:3000/students';
+    this.apiURL = '/api/student';
+  }
+
+  registerStudent(student: Student): void {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    this.http.post<Student>(this.apiURL, student, {headers}).subscribe(res => {
+      console.log(res);
+    });
   }
 
   getStudents(): Observable<Student[]> {
