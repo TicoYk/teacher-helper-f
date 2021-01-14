686e1aed85dd3b9db0fd7975762035d1895b8487
TicoYk <didimoykym@gmail.com>
2021-01-14 11:51:47 -0300

    Implementado listagem e registro de aluno, Close #1

--- src/app/aluno/list/list.component.ts
+++ src/app/aluno/list/list.component.ts
@@ -2,7 +2,7 @@ import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/materi
 import { StudentService } from './../../services/student.service';
 import { ChangeDetectorRef, Component, ViewChild, NgModule } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, MatSortable } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import Student from '../shared/student.model';
 import { NgForm, FormControl, Validators, FormGroupDirective } from '@angular/forms';
@@ -26,6 +26,11 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 })
 export class ListComponent {
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  displayedColumns: string[] = ['id', 'name'];
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+
   studentFormControl = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
@@ -33,11 +38,7 @@ export class ListComponent {
 
   matcher = new MyErrorStateMatcher();
 
-  displayedColumns: string[] = ['id', 'name'];
-  dataSource = new MatTableDataSource<Student>();
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  interval: any;
 
   constructor(private studentService: StudentService, private changeDetectorRefs: ChangeDetectorRef){
     this.studentService.getStudents().subscribe(res => {
@@ -47,9 +48,14 @@ export class ListComponent {
     });
   }
 
-  cadastrar(): void{
+  register(): void{
     const name: string = this.studentFormControl.value;
-    this.studentService.registerStudent(new Student(name));
+    this.studentService.registerStudent(new Student(name)).subscribe( res => {
+      const data = this.dataSource.data;
+      data.push(res);
+      this.dataSource.data = data;
+      alert('Aluno ' + name + ' cadastrado com sucesso!');
+    });
   }
 
 }
