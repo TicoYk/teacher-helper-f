2808abdd50f06824547f57d89d2a038c5e9d52a5
Dídimo Yokoyama <didimoykym@gmail.com>
2021-01-22 18:59:48 -0300

    Finalizado Layout inicial da página Turma

--- src/app/pages/classroom/classroom.component.ts
+++ src/app/pages/classroom/classroom.component.ts
@@ -1,6 +1,7 @@
 import Room from '@models/room.model';
-import { RoomService } from './../../services/room.service';
+import { RoomService } from '@services/room.service';
 import { Component, OnInit } from '@angular/core';
+import Desk from '@models/desk.model';
 
 @Component({
   selector: 'app-classroom',
@@ -11,6 +12,12 @@ export class ClassroomComponent implements OnInit {
 
   rooms: Room[] = [];
   choosenRoom: Room = new Room();
+  choosenRoomIndex?: number;
+  desks: Desk[][] = [];
+  roomLayout = {
+    width: '0px',
+    height: '0px'
+  };
 
   constructor(private roomService: RoomService) {
     this.roomService.getRooms().subscribe( res => {
@@ -21,8 +28,57 @@ export class ClassroomComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSelectChange(value: number){
+  onSelectChange(value: number): void {
+    if (typeof this.rooms[value] === undefined) {
+      alert('Sala não encontrada!');
+      return;
+    }
+    this.choosenRoomIndex = value;
     this.choosenRoom = this.rooms[value];
-    console.log(this.choosenRoom);
+    this.mountRoomLayout();
+  }
+
+  mountRoomLayout(): void{
+    if (this.choosenRoom === undefined){
+      alert('Algum problema ocorreu!');
+      return;
+    }
+    const room = this.choosenRoom;
+    this.desks = [];
+    // tslint:disable-next-line: no-unused-expression
+    room.rows === null || room.rows === undefined ? room.rows = 0 : null;
+    // tslint:disable-next-line: no-unused-expression
+    room.columns === null || room.columns === undefined ? room.columns = 0 : null;
+    this.choosenRoom = room;
+
+    this.roomLayout.height = (100 * room.rows) + 'px';
+
+    this.roomLayout.width = (200 * room.columns) + 'px';
+
+    for (let x = 0; x < room.rows; x++){
+      const deskLine: Desk[] = [];
+      for (let y = 0; y < room.columns; y++){
+        deskLine.push(new Desk(x, y));
+      }
+      this.desks.push(deskLine);
+    }
+  }
+
+  changeColumnValue(value: number): void {
+    if (this.choosenRoom.columns !== undefined){
+      this.choosenRoom.columns += value;
+    }
+    this.mountRoomLayout();
+  }
+
+  changeRowValue(value: number): void {
+    if (this.choosenRoom.rows !== undefined){
+      this.choosenRoom.rows += value;
+    }
+    this.mountRoomLayout();
+  }
+
+  public doSomething(test: any): void {
+    console.log('Emitter: ', test);
   }
 }
