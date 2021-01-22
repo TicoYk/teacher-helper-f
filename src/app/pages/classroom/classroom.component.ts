6848335358e4c4884f57eb39946b3944b2472e2a
Dídimo Yokoyama <didimoykym@gmail.com>
2021-01-21 23:06:49 -0300

    Adicionado layout inicial e busca a api de salas

--- src/app/pages/classroom/classroom.component.ts
+++ src/app/pages/classroom/classroom.component.ts
@@ -1,15 +1,28 @@
+import Room from '@models/room.model';
+import { RoomService } from './../../services/room.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-classroom',
   templateUrl: './classroom.component.html',
-  styleUrls: ['./classroom.component.css']
+  styleUrls: ['./classroom.component.scss']
 })
 export class ClassroomComponent implements OnInit {
 
-  constructor() { }
+  rooms: Room[] = [];
+  choosenRoom: Room = new Room();
+
+  constructor(private roomService: RoomService) {
+    this.roomService.getRooms().subscribe( res => {
+      this.rooms = res;
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSelectChange(value: number){
+    this.choosenRoom = this.rooms[value];
+    console.log(this.choosenRoom);
+  }
 }
