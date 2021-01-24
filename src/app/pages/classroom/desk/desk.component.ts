0d12f432b4ebceb88aad230f218737813381b3ce
Dídimo Yokoyama <didimoykym@gmail.com>
2021-01-23 21:47:25 -0300

    Criada interação entre ClassRoom e Desk

--- src/app/pages/classroom/desk/desk.component.ts
+++ src/app/pages/classroom/desk/desk.component.ts
@@ -1,3 +1,4 @@
+import Student from '@models/student.model';
 import Desk from '@models/desk.model';
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
@@ -5,23 +6,38 @@ import { EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-desk',
   templateUrl: './desk.component.html',
-  styleUrls: ['./desk.component.css']
+  styleUrls: ['./desk.component.scss']
 })
 export class DeskComponent implements OnInit {
 
-  @Output() onTestEmit = new EventEmitter();
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onDeskChange = new EventEmitter<Desk>();
 
   @Input() posX = 0;
   @Input() posY = 0;
   @Input() desk: Desk = new Desk();
+  @Input() nonChoosenStudents: Student[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public testEmit(): void {
-    this.onTestEmit.emit("testing");
+  onSelectChange(value: number): void {
+    if (typeof this.nonChoosenStudents[value] === undefined) {
+      alert('Aluno não encontrado!');
+      return;
+    }
+    this.nonChoosenStudents.forEach( student => {
+      if( student.id === value ){
+        this.desk.student = student;
+      }
+    });
+    this.emitDeskChange();
+  }
+
+  public emitDeskChange(): void {
+    this.onDeskChange.emit(this.desk);
   }
 
 }
